9358dc4676444adb2ecb72ef792d1ef7cfd03681
Michael O'Leary <michaeloleary@u.northwestern.edu>
2023-04-13 22:38:26 -0500

    Works now for first API endpoint

--- bird_alchemy.py
+++ bird_alchemy.py
@@ -39,39 +39,42 @@ def welcome():
         f"/api/v1.0/obs_by_species_and_date/species/start/end<br/>"
     )
 
+# Helper function to convert SQL Alchemy ORM rows to dictionaries
+def object_as_dict(row):
+    result = {}
+    for column in inspect(row).mapper.column_attrs:
+        result[column.key] = getattr(row, column.key)
+    return result
+
 ####
 # /api/v1.0/obs_by_species_and_date
+# Example URL:  http://127.0.0.1:5000/api/v1.0/obs_by_species_and_date/Bald%20Eagle/2011-01-19/2011-01-20
 ####  
-@app.route("/api/v1.0/obs_by_species_and_date/species/start/end")
+@app.route("/api/v1.0/obs_by_species_and_date/<species>/<start>/<end>")
 def obs_by_species_and_date(species, start, end):
 
     # Create session from Python to the DB:
     session = Session(engine)
 
     # Get data for the selected species and date ranges
-    # 
-    # Returns a list of tuples, like this:
-    # [('2016-08-24', 0.08), ('2016-08-24', 2.15), ('2016-08-24', 2.28), ...
-    bird_data = session.query(Birds.COMMON_NAME, Birds.OBSERVATION_DATE, Birds.LATITUDE, Birds.LONGITUDE).\
+    bird_data = session.query(Birds).\
         filter(
             Birds.OBSERVATION_DATE >= (start),
             Birds.OBSERVATION_DATE <= (end),
-            Birds.COMMON_NAME
-        ).order_by(Birds.OBSERVATION_DATE).all()
+            Birds.COMMON_NAME == (species),
+        ).all()
+
+    # Convert each row into a dictionary for jsonify:
+    bird_data = [object_as_dict(row) for row in bird_data]
+
+    # bird_data = [row._asdict() for row in bird_data]  # Convert 'Row' objects into dictionaries
 
     # Close the session as soon as it's no longer needed:
     session.close()
-
-    # Convert list of tuples to a dictionary:
-    birds_dict = {}
-    birds_dict = dict(bird_data)
-    print(bird_data)
 
     # Return the JSON representation of your dictionary.
     return jsonify(bird_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-# obs_by_species_and_date("Bald Eagle", "2011-01-19", "2011-01-20")
 
